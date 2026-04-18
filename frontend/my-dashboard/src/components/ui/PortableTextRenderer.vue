f3db57d7090a9a2d2b9684e82cd76b79e05be58e
dehlix <deborahligorio@gmail.com>
2026-04-18 19:43:32 +0200

    PortableText rendering with custom link handler

---
+++ frontend/my-dashboard/src/components/ui/PortableTextRenderer.vue
@@ -0,0 +1,20 @@
+<template>
+  <PortableText :value="body" :components="components" />
+</template>
+
+<script setup>
+import { h } from "vue";
+import { PortableText } from "@portabletext/vue";
+
+const props = defineProps(["body"]);
+
+const components = {
+  marks: {
+    link: ({ value }, { slots }) => {
+      const target = value?.blank ? "_blank" : "_self";
+      const rel = value?.blank ? "noopener noreferrer" : undefined;
+      return h("a", { href: value?.href, target, rel }, slots.default?.());
+    },
+  },
+};
+</script>
