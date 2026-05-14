9d6c2ecd98e58cf5690ca2dcf08093fefa723b0f
dehlix <deborahligorio@gmail.com>
2026-05-14 10:47:49 +0200

    SEO configuration

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  site: "https://www.someseeds.net",
+  integrations: [vue()],
 });
