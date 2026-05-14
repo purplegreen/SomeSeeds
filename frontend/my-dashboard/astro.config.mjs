7a2242339df2bfc095d8d0c148d22b9b8e896d43
dehlix <deborahligorio@gmail.com>
2026-05-14 11:05:25 +0200

    added site map

--- frontend/my-dashboard/astro.config.mjs
+++ frontend/my-dashboard/astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import vue from '@astrojs/vue';
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue(), sitemap()],
 });
